c1bf185659308e2dfc7c0767fe0e6fe21996fbda
Saif ur Rehman <buttsaif539@gmail.com>
2025-03-24 15:53:21 +0500

    Make the profile Page Private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 function App() {
   return (
     <Router>
@@ -16,7 +17,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,26 +2,40 @@ import React from 'react'
 import logo from '/logo.png'
 import { IoSearch } from "react-icons/io5";
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+
 function Header() {
+  const {currentUser} = useSelector((state) => state.user);
   return (
     <header className='bg-slate-200 p-3 text-slate-600 shadow-md'> 
-    <div className='flex items-center justify-between max-w-6xl mx-auto'>
-    <Link to={'/'} className='w-28 h-12 flex items-center justify-center'>
-<img  className='w-full h-fit' src={logo} alt="" />
-    </Link>
-    <form className='bg-slate-300 p-3 rounded-lg flex items-center'>
-      <input className='text-slate-600  w-24 sm:w-62 bg-transparent  outline-none' type="text" placeholder='Search.....' />
-      <IoSearch className='text-slate-700 text-lg' />
-
-    </form>
-    <nav className='text-slate-600'>
-      <ul className='flex space-x-6'>
+      <div className='flex items-center justify-between max-w-6xl mx-auto'>
+        <Link to={'/'} className='w-28 h-12 flex items-center justify-center'>
+          <img className='w-full h-fit' src={logo} alt="" />
+        </Link>
+        <form className='bg-slate-300 p-3 rounded-lg flex items-center'>
+          <input className='text-slate-600 w-24 sm:w-62 bg-transparent outline-none' type="text" placeholder='Search.....' />
+          <IoSearch className='text-slate-700 text-lg' />
+        </form>
+        <nav className='text-slate-600'>
+          <ul className='flex space-x-6'>
             <li className='hover:underline'><Link to={'/'}>Home</Link></li>
             <li className='hover:underline'><Link to={'/about'}>About</Link></li>
-            <li className='hover:underline'><Link to={'/sign-up'}>Sign Up</Link></li>
-      </ul>
-    </nav>
-    </div>
+            <li className='hover:underline'>
+              {currentUser ? (
+                <Link to={'/profile'}>
+                  <img 
+                    src={currentUser.avatar} 
+                    alt="Profile" 
+                    className='w-8 h-8 rounded-full object-cover'
+                  />
+                </Link>
+              ) : (
+                <Link to={'/sign-in'}>Sign In</Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
